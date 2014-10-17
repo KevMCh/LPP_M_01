a2b522ed55ed12e95082a23393cbb6ec5b06f04f
Kevin Martín <alu0100774655@ull.edu.es>
2014-10-17 12:33:09 +0100

    Primeras operaciones realizadas

---
+++ src/lib/complejo.rb
@@ -0,0 +1,39 @@
+class Complejo
+	
+	attr_reader :r, :i
+
+	def initialize(r,i)
+
+		@r, @i = r, i
+	end
+
+	def to_s
+
+    		"#{@r}, #{@i} i"
+  	end
+
+	def suma(oper1, oper2)
+
+
+		@r = oper1.r + oper2.r
+		@i = oper1.i + oper2.i
+
+	end
+
+	def multipl(oper1, oper2)
+
+		@r = oper1.r * oper2.r - oper1.i * oper2.i
+		@i = oper1.r * oper2.i + oper1.r * oper2.r
+
+	end
+
+
+	def prodPorEsca(escalar)
+
+    		@r *= escalar
+		@i *= escalar
+
+  	end
+
+end
+
